4949090fd5e0cf8afd4f667c3a2196233f0011fd
PanPole <77209709+PanPolePL@users.noreply.github.com>
2023-02-18 19:24:01 +0100

    player can now fire 'bullets'

--- core/src/com/twojstary/javagame/JavaGame.java
+++ core/src/com/twojstary/javagame/JavaGame.java
@@ -13,6 +13,8 @@ public class JavaGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Rectangle player;
 	Texture playerImage;
+	Rectangle ammo;
+	Texture ammoImage;
 	
 	@Override
 	public void create () {
@@ -23,6 +25,13 @@ public class JavaGame extends ApplicationAdapter {
 		player.y = 0;
 		player.width = 150;
 		player.height = 125;
+
+		ammoImage = new Texture(Gdx.files.internal("ammo.png"));
+		ammo = new Rectangle();
+		ammo.x = 512;
+		ammo.y = 9999;
+		ammo.width = 70;
+		ammo.height = 70;
 	}
 
 	@Override
@@ -30,6 +39,7 @@ public class JavaGame extends ApplicationAdapter {
 		ScreenUtils.clear(0, 0, 0, 1);
 		batch.begin();
 		batch.draw(playerImage, player.x, player.y);
+		batch.draw(ammoImage, ammo.x, ammo.y);
 		batch.end();
 
 		//user input
@@ -41,14 +51,25 @@ public class JavaGame extends ApplicationAdapter {
 			player.x += 300 * Gdx.graphics.getDeltaTime();
 			playerImage=new Texture(Gdx.files.internal("player_r.png"));
 		}
-		if(Gdx.input.isKeyPressed(Input.Keys.UP)) player.y += 300 * Gdx.graphics.getDeltaTime();
-		if(Gdx.input.isKeyPressed(Input.Keys.DOWN)) player.y -= 300 * Gdx.graphics.getDeltaTime();
+		//if(Gdx.input.isKeyPressed(Input.Keys.UP)) player.y += 300 * Gdx.graphics.getDeltaTime();
+		//if(Gdx.input.isKeyPressed(Input.Keys.DOWN)) player.y -= 300 * Gdx.graphics.getDeltaTime();
+
+		//firing
+		if(Gdx.input.isKeyJustPressed(Input.Keys.F)) {
+			if(ammo.y >= 720 + ammo.height) {
+				ammo.x=player.x+50;
+				ammo.y=player.y+player.height;
+			}
+		}
 
 		//borders
 		if(player.x < 0) player.x = 0;
 		if(player.x > 1280-player.width) player.x = 1280-player.width;
-		if(player.y < 0) player.y = 0;
-		if(player.y > 720-player.width) player.y = 720-player.width;
+		//if(player.y < 0) player.y = 0;
+		//if(player.y > 720-player.width) player.y = 720-player.width;
+
+		//ammo
+		ammo.y+=10;
 	}
 	
 	@Override
